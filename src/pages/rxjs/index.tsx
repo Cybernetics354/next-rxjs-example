d938d41acba54430d8ddc7c7db75d7dc2ac63e53
Hanif Mifta <hanif@jakpat.net>
2023-02-02 00:27:24 +0700

    Initial Commit

---
+++ src/pages/rxjs/index.tsx
@@ -0,0 +1,76 @@
+import { useListenObjectRx } from "@/utils/hooks";
+import { createContext, useContext, useRef, useState } from "react";
+import { BehaviorSubject } from "rxjs";
+
+interface IState {
+  name: string;
+  age: number;
+}
+
+const ControllerContext = createContext<BehaviorSubject<IState>>(
+  {} as BehaviorSubject<IState>
+);
+
+export default function RxjsPage() {
+  const controller = useRef(
+    new BehaviorSubject<IState>({
+      name: "Lorem",
+      age: 20,
+    })
+  ).current;
+
+  return (
+    <ControllerContext.Provider value={controller}>
+      <Name />
+      <Age />
+      <button
+        onClick={() => {
+          controller.next({
+            ...controller.value,
+            age: controller.value.age + 1,
+          });
+        }}
+      >
+        Increment Age
+      </button>
+      <div style={{ height: 40 }} />
+      <UseStateComponent />
+    </ControllerContext.Provider>
+  );
+}
+
+function Name() {
+  const controller = useContext(ControllerContext);
+  const { name } = useListenObjectRx(controller, ["name"]);
+
+  console.log("NAME ::", "Re-rendering");
+
+  return (
+    <>
+      <p>The name is {name}</p>
+    </>
+  );
+}
+
+function Age() {
+  const controller = useContext(ControllerContext);
+  const { age } = useListenObjectRx(controller, ["age"]);
+
+  console.log("Age ::", "Re-rendering");
+  return <p>The age is {age}</p>;
+}
+
+function UseStateComponent() {
+  const [state, setState] = useState(0);
+
+  console.log("UseStateComponent :: ", "Re-rendering");
+
+  return (
+    <div>
+      <p>Use State Component : {state}</p>
+      <button onClick={() => setState(state + 1)}>
+        Use State Component Increment
+      </button>
+    </div>
+  );
+}
---
+++ src/utils/hooks.tsx
@@ -0,0 +1,65 @@
+import { useState, useLayoutEffect } from "react";
+import { BehaviorSubject } from "rxjs";
+
+function useListenRx<T>(controller: BehaviorSubject<T>) {
+  const [state, setState] = useState(controller.value);
+  useLayoutEffect(() => {
+    const subscription = controller.subscribe((newValue) => {
+      if (newValue === state) return;
+      setState(newValue);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return state;
+}
+
+function useListenObjectRx<T extends Object, F extends Array<keyof T>>(
+  controller: BehaviorSubject<T>,
+  fields: F
+): Pick<T, F[number]> {
+  const getSplicedValue = (value: T) => {
+    const data = { ...value };
+    Object.keys(data).forEach((field) => {
+      if (fields.includes(field as keyof T)) return;
+      delete data[field as keyof T];
+    });
+
+    return data;
+  };
+
+  const initialState = getSplicedValue(controller.value);
+  const [state, setState] = useState<Pick<T, F[number]>>(initialState);
+
+  useLayoutEffect(
+    () => {
+      const subscription = controller.subscribe((newValue) => {
+        let isChanged = false;
+
+        fields.map((field) => {
+          if (state[field] === newValue[field] || isChanged) return;
+          isChanged = true;
+        });
+
+        if (!isChanged) return;
+
+        const splicedNewValue = getSplicedValue(newValue);
+        setState(splicedNewValue);
+      });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return state;
+}
+
+export { useListenRx, useListenObjectRx };
